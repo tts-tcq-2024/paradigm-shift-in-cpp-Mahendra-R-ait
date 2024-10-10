4b8863357935bfa9bdf26bdc0ddf987df420119a
Mahendra R <83295768+Mahendra-R-ait@users.noreply.github.com>
2024-10-10 16:26:45 +0530

    Update temp_check.cpp

--- temp_check.cpp
+++ temp_check.cpp
@@ -9,10 +9,10 @@ using namespace std;
 float Temp_warning_tolerange = (TOLERANCE_PERCENTAGE * TEMP_MAX);
 bool isTempOK(float temperature){
 if (temperature < (0 + Temp_warning_tolerange)){
-  cout<<"Warning: Approaching Low Temperature\n;
+  cout<<"Warning: Approaching Low Temperature\n";
 }
 else if ( temperature > (45 - Temp_warning_tolerange){
-  cout<<""Warning: Approaching High Temperature\n;
+  cout<<"Warning: Approaching High Temperature\n";
 }
 else{
     //do nothing
